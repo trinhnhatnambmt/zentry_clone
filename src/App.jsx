ceb4bfd6f6c47275aef3f05ee0cf98ec3f6ac9de
Huy Trịnh <trinhnhathuy3@gmail.com>
2025-05-06 16:19:46 +0700

    features

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Hero from "./components/Hero";
 import About from "./components/About";
+import Navbar from "./components/Navbar";
+import Features from "./components/Features";
 
 const App = () => {
     return (
-        <div className="relative overflow-x-hidden min-h-screen w-screen">
+        <div className="relative overflow-x-hidden min-h-screen w-screen ">
+            <Navbar />
             <Hero />
             <About />
+            <Features />
         </div>
     );
 };
